cfd386d6a045f55269d9da0672b40454c4f2a715
sbiswa338 <112734448+sbiswa338@users.noreply.github.com>
2022-11-07 16:06:27 -0600

    Lesson 15

--- scripts/script1.js
+++ scripts/script1.js
@@ -4,10 +4,25 @@ console.log(able);
 let msg;
 if(able == "My First Paragraph") {
     msg = "My First change";
-
 }
 
 let beta = document.getElementById("first");
 beta.style.color = "darkcyan";
 beta.style.fontSize = "20px";
+
 document.getElementById("first").innerText=msg;
+
+// by class name
+let you = document.getElementsByClassName('cool');
+console.log(you);
+
+// lets change all of the elements with classname cool to a different color
+let heads = document.querySelectorAll('.cool');
+
+heads.forEach(delta => {
+    delta.style.color = 'orange';
+});
+
+// Example by element 
+let para = document.querySelectorAll('p');
+console.log(para); 
